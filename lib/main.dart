ea93f1beb1739280ee9ed534cb92e9b2ffa44e35
Himanshu Hiware <hiwarehh@rknec.edu.in>
2023-03-03 20:58:38 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:gymo/splash_screens/splash_screen.dart';
 void main() {
   runApp(const MyApp());
 }
-
+//himanshu
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
