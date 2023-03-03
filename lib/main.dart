c29fae0468c6eb603b8afb5b476d2847be508ece
yavd1 <108459578+yavd1@users.noreply.github.com>
2023-03-03 21:02:16 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ import 'package:gymo/splash_screens/splash_screen.dart';
 void main() {
   runApp(const MyApp());
 }
-//himanshu
+//commit by vinay
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
